ce1ade9b046fb40396c82319442d528817b29d10
kittuar <45809267+kittuar@users.noreply.github.com>
2022-09-05 17:41:45 -0600

    Update Form2.cs

--- Form2.cs
+++ Form2.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -11,13 +11,13 @@ using System.Windows.Forms;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 using TextBox = System.Windows.Forms.TextBox;
 
-namespace KeyApp 
+namespace KeyApp
 {
     public partial class Form2 : Form
     {
         public Form2()
         {
-           
+
             InitializeComponent();
         }
 
@@ -30,7 +30,7 @@ namespace KeyApp
 
         private void Form2_Load(object sender, EventArgs e)
         {
-            
+
 
             wplayer.URL = "C:\\Users\\Emerald\\Desktop\\Sound ON\\Nyakuza Manholes.mp3";
             wplayer.controls.play();
@@ -57,10 +57,10 @@ namespace KeyApp
         }
 
         private int duration = 900;
-        
-            
 
-        
+
+
+
         private void count_down(object sender, EventArgs e)
         {
 
@@ -85,7 +85,7 @@ namespace KeyApp
         {
             wplayer.controls.stop();
 
-            
+
 
         }
 
@@ -94,6 +94,5 @@ namespace KeyApp
 
     }
 
-        
-    }
 
+}
